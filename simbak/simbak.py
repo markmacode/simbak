1b5bff83631153911f3eb6b169de7068a25baac2
Mark Bromell <markbromell.business@gmail.com>
2020-06-08 17:45:11 +0100

    Started 'better logging'

--- simbak/simbak.py
+++ simbak/simbak.py
@@ -24,20 +24,22 @@ def _filter_paths(paths: list, create=False) -> list:
                 f'{normpath} doesn\'t exist, creating directory with that path.')
             _os.makedirs(normpath)
             filtered_paths.append(normpath)
+        else:
+            _logging.warning(f'Failed to access {normpath}, it doesn\'t exist')
 
     return filtered_paths
 
 
-def _unique_file_name(file_name: str) -> str:
+def _unique_file_name(name: str) -> str:
     """Creates a unique file name by suffixing date and time"""
     time_suffix = _datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
-    file_name = f'{file_name}--{time_suffix}.tar.gz'
+    file_name = f'{name}--{time_suffix}.tar.gz'
     return file_name
 
 
-def _create_backup(sources: list, destination: str, name: str, compression_level: int):
+def _create_backup(sources: list, destination: str, file_name: str, compression_level: int):
     """Creates the backup file in the first destination"""
-    first_path = _os.path.join(destination, name)
+    first_path = _os.path.join(destination, file_name)
 
     try:
         tar = _tarfile.open(first_path, 'x:gz', compresslevel=compression_level)
@@ -52,9 +54,9 @@ def _create_backup(sources: list, destination: str, name: str, compression_level
                     f'Couldn\'t compress {source}, pemission denied.')
 
         tar.close()
-        _logging.info(f'Saved backup {name} to {destination}')
+        _logging.info(f'Saved backup {file_name} to {destination}')
     except FileExistsError:
-        _logging.error(f'Failed to create backup {name}, file already exists')
+        _logging.error(f'Failed to create backup {file_name}, file already exists')
 
     return first_path
 
@@ -68,10 +70,12 @@ def _distribute_backup(backup_path: str, destinations: list, name: str):
 
 
 def backup(sources: list, destinations: list, name: str, compression_level: int = 6):
+    _logging.info(f'Starting backup {name}')
     sources = _filter_paths(sources)
     destinations = _filter_paths(destinations, create=True)
-    name = _unique_file_name(name)
+    file_name = _unique_file_name(name)
+    _logging.info(f'Backup file name will be {name}')
 
-    first_path = _create_backup(sources, destinations[0], name,
+    first_path = _create_backup(sources, destinations[0], file_name,
                                 compression_level)
-    _distribute_backup(first_path, destinations[1:], name)
+    _distribute_backup(first_path, destinations[1:], file_name)
